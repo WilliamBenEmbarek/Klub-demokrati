93234dcc0b978c665224357a40f5113ac37d9a6c
William Ben Embarek <embar@localhost.localdomain>
2022-06-10 12:23:06 +0200

    Biggo move

--- config-overrrides.js
+++
@@ -1,9 +0,0 @@
-module.exports = function override (config, env) {
-    console.log('override')
-    let loaders = config.resolve
-    loaders.fallback = {
-        "url": require.resolve("url/")
-    }
-    
-    return config
-}
