1ec25f42a58781b319dbd802d4834974d7de9d46
william ben embarek <william.benembarek@gmail.com>
2022-06-21 10:58:07 +0200

    Reactive CSS for mobile + basic admin panel
    Admin panel requires backend planning for functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import ClubDemokrati from './Components/ClubDemokrati.js';
+import AdminPanel from './Components/AdminPanel';
 function App() {
+
+  if (window.location.pathname === "/admin") {
+    return(
+      <AdminPanel/>
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+      <meta name="viewport" content="width=device-width, initial-scale=1"/> 
       <ClubDemokrati/>
       </header>
     </div>
---
+++ frontend/src/Components/AdminPanel.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './AdminPanel.css'
+
+function AdminPanel() {
+
+    return(
+        <div className="ControlPanelButtons">
+            <button>Start Game</button>
+            <button>Stop Game</button>
+        </div>
+    )
+}
+
+export default AdminPanel;
